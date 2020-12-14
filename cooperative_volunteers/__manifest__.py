d684b59280f54a213f6cbca5dc374d10e938570b
Rosa Ortega <odoo@lym.com.mx>
2020-12-14 19:00:54 +0000

    add cooperative volunteers

---
+++ cooperative_volunteers/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Cooperative Volunteers',
+    'summary' : 'App for cooperative volunteers',
+    'description' : 'Modulos for cooperative volunteers',
+    'author' : 'Rosa Ortega',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends' : ['base'],
+    'data' : [
+        #'security/cooperative_volunteers_security.xml',
+        #'security/ir.model.access.csv',
+        #'views/cv_menuitems.xml',
+        #'views/cv_views.xml',
+    ],
+    'demo' : [
+        #'demo/cv_demo.xml'
+    ],
+}
